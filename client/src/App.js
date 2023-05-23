1073a6ce86bb079c3bf8fa19d440f03a5e635257
Enes Yarali <s302845@studenti.polito.it>
2023-05-23 22:08:22 +0200

    Themes Setup Done

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,27 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "scenes/homePage"; //Perks of setting a jscongif.json we dont have to setup relative routes constantly.
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline , ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
+import { themeSettings } from "theme";
+
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme  = useMemo(() => createTheme(themeSettings(mode)) , [mode])
   return (
     <div className="app">
       <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/> {/*CSS RESET to basic css for materialUI.Disables browsers predefined CSS.*/}
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile/:userId" element={<ProfilePage />} />
         </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
